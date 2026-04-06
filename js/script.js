5d5b4d707b89d2325259bfad6b1a7e37fd45986e
Alekoo08 <alexsandro.9581982@aluno.mg.gov.br>
2026-04-06 16:09:59 +0000

    alteraçoes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let likeCount = 0;
 let curtido = false; // flag booleana
 
+let deslikeCount = 0;
+let descurtido = false; // flag booleana
+
 function curtir() {
 
  if(curtido == false){
@@ -13,7 +16,20 @@ function curtir() {
     document.getElementById("likeCount").innerText = likeCount;
  }
 
+   function descurtir() {
+}
+ if(descurtido == false){
+    deslikeCount++;
+    descurtido = true;
+    document.getElementById("deslikeCount").innerText = deslikeCount;
+ }else{
+    deslikeCount--;
+    descurtido = false;
+    document.getElementById("deslikeCount").innerText = deslikeCount;
+ }
+
   
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
+document.getElementById("deslikeBtn").addEventListener("click", descurtir);
